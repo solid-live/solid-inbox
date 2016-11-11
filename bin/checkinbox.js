3aec3e8606a526a4ba6f8b1052f60c9094133cbf
Melvin Carvalho <melvincarvalho@gmail.com>
2016-11-11 12:59:27 +0100

    Cleanup

--- bin/checkinbox.js
+++ bin/checkinbox.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var shell = require('rdf-shell')
-$rdf = require('rdflib')
+var $rdf = require('rdflib')
 var util = shell.util
 
 /**
@@ -16,9 +16,13 @@ function bin (argv) {
   }
   console.log('Scanning inbox:', uri)
   shell.ls(uri, function (err, arr) {
-    for (var i = 0; i < arr.length; i++) {
-      var file = arr[i]
-      shell.cat(file, processResult(file))
+    if (!err) {
+      for (var i = 0; i < arr.length; i++) {
+        var file = arr[i]
+        shell.cat(file, processResult(file))
+      }
+    } else {
+      console.error(err)
     }
   })
 }
@@ -27,17 +31,11 @@ function processResult (file) {
   return function (err, res) {
     if (!err) {
       var g = $rdf.graph()
-      var f = $rdf.fetcher(g, 2000)
-
-      var t = $rdf.parse(res, g, file, 'text/turtle')
 
       var pt = util.primaryTopic(g, file)
-      // console.log('pt', pt)
-
       var SIOC = $rdf.Namespace('http://rdfs.org/sioc/ns#')
 
       var isPost = util.is(g, pt, SIOC('Post').uri)
-      // console.log('isPost', isPost)
       if (isPost) {
         console.log('SIOC Post found!', pt)
         console.log(res)
