0b73efd38ab98064a628333359ef93be92f94142
Melvin Carvalho <melvincarvalho@gmail.com>
2016-11-12 14:48:22 +0100

    Most posts to timeline

--- bin/checkinbox.js
+++ bin/checkinbox.js
@@ -3,6 +3,7 @@
 var debug = require('debug')('sl-inbox:checkinbox')
 var shell = require('rdf-shell')
 $rdf = require('rdflib')
+var path = require('path')
 var util = shell.util
 
 /**
@@ -58,7 +59,7 @@ function processResult (file) {
  * @param  {object} store rdflib store
  * @param  {[type]} uri   uri of the post
  */
-function processPost(store, uri) {
+function processPost (store, uri) {
   console.log('SIOC Post found!', uri)
   var v = validatePost(store, uri)
   if (v) {
@@ -79,7 +80,7 @@ function processPost(store, uri) {
  * @param  {[type]} uri   uri of the post
  * @return true if post is valid
  */
-function validatePost(store, uri) {
+function validatePost (store, uri) {
   var ret = true
 
   return ret
@@ -90,14 +91,28 @@ function validatePost(store, uri) {
  * @param  {object} store rdflib store
  * @param  {[type]} uri   uri of the post
  */
-function moveToTimeline(store, uri) {
+function moveToTimeline (store, uri) {
   var SOLID = $rdf.Namespace('http://www.w3.org/ns/solid/terms#')
-  var timeline = store.any($rdf.sym(uri), SOLID('timeline'))
-  debug('timeline', timeline)
-  var st = store.statementsMatching($rdf.sym(uri))
-  debug('st', st)
-}
+  var DCT = $rdf.Namespace('http://purl.org/dc/terms/')
 
+  var timeline = store.any($rdf.sym(uri), SOLID('timeline'))
+  var created = store.any($rdf.sym(uri), DCT('created'))
+
+  debug('timeline', timeline)
+  debug('created', created)
+  debug('source', uri)
+  debug('basename', path.basename(uri))
+
+  if (timeline) {
+    shell.mv(uri.split('#')[0], timeline.uri + '/' + path.basename(uri), function (err, ret) {
+      if (!err) {
+        debug(ret)
+      } else {
+        debug(err)
+      }
+    })
+  }
+}
 
 // If one import this file, this is a module, otherwise a library
 if (require.main === module) {
